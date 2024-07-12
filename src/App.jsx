aebebe5734b9585628bab7dd57b678aa8cf21afa
CodeWithMomin <mominzahoor11@gmail.com>
2024-07-12 23:21:36 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import './App.css'
+import axios from 'axios'
+const App = () => {
+  const [question,setquestion]=useState("")
+  const [answer,setanswer]=useState("")
+async function genrateAnswer()
+{
+  setanswer("loading");
+  const response=await axios({
+    url:"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyBs1GLNuLlL1vyo7wk1SX_zErdwdpGwM50",
+    method:'post',
+    data:{"contents":[{"parts":[{"text":question}]}]}
+  })
+  setanswer(response['data']['candidates'][0]['content']['parts'][0]['text']);
+}
+  return (
+   <>
+   <div className="container">
+   <div className='box'>
+      <div className="imagediv">
+        <img src="https://cdn-icons-png.flaticon.com/128/11592/11592357.png"  />
+      </div>
+      <div className="heading">
+        <h3>How Can I Help You Today?</h3>
+      </div>
+      <div className="inputdiv">
+        <input id='input' type="text" placeholder='Message ChatBot' value={question} onChange={(e)=>{
+          setquestion(e.target.value)
+        }} />
+      </div>
+      <div className="btndiv">
+        <button onClick={genrateAnswer}>
+          <img id='btnimg' src="https://cdn-icons-png.flaticon.com/128/5974/5974717.png" alt="" />
+        </button>
+      </div>
+    </div>
+    <pre className="response">
+      {answer}
+    </pre>
+   </div>
+   </>
+
+  )
+}
+
+export default App
